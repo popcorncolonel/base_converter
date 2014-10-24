26ef889a70019125d873881325e598865781d07c
popcorncolonel <popcorncolonel@gmail.com>
2014-10-23 22:26:13 -0400

    Deleted unnecessary things.

--- backup/calculator.h
+++
@@ -1,7 +0,0 @@
-/*
- * computes commands in string form and returns a base 10 integer
- *
- * ex:  input:   ["44" "+" "3" "-" "8"] (base 10)
- *      output:  39
- */
-int calculate(char **cmds, int num_cmds, int base);
--- backup/readline.h
+++
@@ -1,2 +0,0 @@
-/* returns all the stuff on a line in char form, ignoring whitespace!! */
-char **readline(int *num_strings);
